86d45e67267b1e298cf7b87f986edd56776fb9b5
Patrick José Pereira <patrickelectric@gmail.com>
2018-07-21 16:33:15 -0300

    main: Remove unused imports
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import sys
 
 from PySide2.QtWidgets import QApplication
-from PySide2.QtQuick import QQuickView
 from PySide2.QtQml import QQmlApplicationEngine
-from PySide2.QtCore import QUrl
 from PySide2.QtQml import qmlRegisterType
 
 from Map import *
