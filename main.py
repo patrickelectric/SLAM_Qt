b536813a31d62eb3415bce47538995effa0b9534
Patrick José Pereira <patrickelectric@gmail.com>
2018-09-09 19:03:26 -0300

    Main: Handle High DPI
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 
+from PySide2.QtCore import Qt, QCoreApplication
 from PySide2.QtWidgets import QApplication
 from PySide2.QtQml import QQmlApplicationEngine
 from PySide2.QtQml import qmlRegisterType
@@ -8,6 +9,9 @@ from Map import *
 from Bot import *
 
 app = QApplication(sys.argv)
+
+QApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
+QCoreApplication.setAttribute(Qt.AA_UseHighDpiPixmaps)
 
 qmlRegisterType(Map, 'Map', 1, 0, 'Map')
 qmlRegisterType(Bot, 'Bot', 1, 0, 'Bot')
