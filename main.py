ef3ba2aa6d6fd1fd26bb6a733fe9df34aa06ace7
Patrick José Pereira <patrickelectric@gmail.com>
2018-11-06 22:51:24 -0200

    Move content to main function
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- main.py
+++ main.py
@@ -8,15 +8,19 @@ from PySide2.QtQml import qmlRegisterType
 from Map import *
 from Bot import *
 
-sys.argv += ['--style', 'material']
-app = QApplication(sys.argv)
+def main():
+    sys.argv += ['--style', 'material']
+    app = QApplication(sys.argv)
 
-QApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
-QCoreApplication.setAttribute(Qt.AA_UseHighDpiPixmaps)
+    QApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
+    QCoreApplication.setAttribute(Qt.AA_UseHighDpiPixmaps)
 
-qmlRegisterType(Map, 'Map', 1, 0, 'Map')
-qmlRegisterType(Bot, 'Bot', 1, 0, 'Bot')
+    qmlRegisterType(Map, 'Map', 1, 0, 'Map')
+    qmlRegisterType(Bot, 'Bot', 1, 0, 'Bot')
 
-engine = QQmlApplicationEngine('main.qml')
+    engine = QQmlApplicationEngine('main.qml')
 
-sys.exit(app.exec_())
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
